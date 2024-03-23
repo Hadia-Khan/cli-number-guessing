4f1dc14199c9bcb85860de003c05c083b832cd67
HadiaKhan77 <160766759+HadiaKhan77@users.noreply.github.com>
2024-03-24 00:01:47 +0500

    Added number guessing game

---
+++ index.ts
@@ -0,0 +1,14 @@
+import inquirer from "inquirer";
+let randomNumber = Math.floor(Math.random() * 6 + 1);
+let answer = await inquirer.prompt([
+  {
+    message: "Please guess a number between 1-6",
+    type: "number",
+    name: "userGuessedNumber",
+  },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+  console.log("Congratulations! You Guessed The Right Number");
+} else {
+  console.log("You Guessed Wrong Number");
+}
